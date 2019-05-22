62dfafae0aa3c754a63f35715c78586b004c3f14
iamwangyabin <yabinwango@gmail.com>
2019-05-22 21:42:46 +0800

    add LK

--- LK.py
+++ LK.py
@@ -6,36 +6,18 @@ STAGE_SECOND_FRAME = 1
 STAGE_DEFAULT_FRAME = 2
 kMinNumFeature = 1500
 
-def getKptsDescs(feat_path, img_path):
-	path = img_path.split('/')
-	npz_path = feat_path + path[1] + '.npz'
-	data = np.load(npz_path)
-	des = data['descs']
-	kpt = data['kpts']
-	return kpt, des
+lk_params = dict(winSize  = (21, 21), 
+				maxLevel = 3,
+             	criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# image_ref -- String -- 前一帧
-# image_cur
-def featureTracking_(image_ref, image_cur):
-	feat_path = './Data/rgb_feats/'
-	k1, d1 = getKptsDescs(feat_path, image_ref)
-	k2, d2 = getKptsDescs(feat_path, image_cur)
-	# BFMatcher
-	FLANN_INDEX_KDTREE = 0
-	index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-	search_params = dict(checks=50)
-	flann = cv2.FlannBasedMatcher(index_params,search_params)
-	matchers = flann.knnMatch(d1,d2,k=2)
-    # 相似列表
-	Match = []
-	for m,n in matchers:
-		if m.distance <  0.80*n.distance:
-			Match.append(m)
-	p1 = [kpp.queryIdx for kpp in Match[:]]
-	p2 = [kpp.trainIdx for kpp in Match[:]]
-	post1 = np.int32([k1[pp] for pp in p1])
-	post2 = np.int32([k2[pp] for pp in p2]) 
-	return post1, post2
+def featureTracking(image_ref, image_cur, px_ref):
+	kp2, st, err = cv2.calcOpticalFlowPyrLK(image_ref, image_cur, px_ref, None, **lk_params)  #shape: [k,2] [k,1] [k,1]
+
+	st = st.reshape(st.shape[0])
+	kp1 = px_ref[st == 1]
+	kp2 = kp2[st == 1]
+
+	return kp1, kp2
 
 
 class PinholeCamera:
@@ -50,47 +32,50 @@ class PinholeCamera:
 		self.distortion = (abs(k1) > 0.0000001)
 		self.d = [k1, k2, p1, p2, k3]
 
+
 class VisualOdometry:
-	def __init__(self, cam, imgList):
+	def __init__(self, cam):
 		self.frame_stage = 0
 		self.cam = cam
 		self.new_frame = None
 		self.last_frame = None
-		self.imgList = imgList
 		self.cur_R = None
 		self.cur_t = None
 		self.px_ref = None
 		self.px_cur = None
 		self.focal = cam.fx
-		# principal point
 		self.pp = (cam.cx, cam.cy)
+		self.trueX, self.trueY, self.trueZ = 0, 0, 0
+		self.detector = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
 
 	def processFirstFrame(self):
+		self.px_ref = self.detector.detect(self.new_frame)
+		self.px_ref = np.array([x.pt for x in self.px_ref], dtype=np.float32)
 		self.frame_stage = STAGE_SECOND_FRAME
 
 	def processSecondFrame(self):
-		# self.px_ref, self.px_cur = featureTracking(self.last_frame, self.new_frame, self.px_ref)
-		self.px_ref, self.px_cur = featureTracking_(self.last_frame, self.new_frame)
+		self.px_ref, self.px_cur = featureTracking(self.last_frame, self.new_frame, self.px_ref)
 		E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC, prob=0.999, threshold=1.0)
 		_, self.cur_R, self.cur_t, mask = cv2.recoverPose(E, self.px_cur, self.px_ref, focal=self.focal, pp = self.pp)
 		self.frame_stage = STAGE_DEFAULT_FRAME 
 		self.px_ref = self.px_cur
 
+
 	def processFrame(self, frame_id):
-		self.px_ref, self.px_cur = featureTracking_(self.last_frame, self.new_frame)
-		E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC, prob=0.999, threshold=1.0)
+		self.px_ref, self.px_cur = featureTracking(self.last_frame, self.new_frame, self.px_ref)
+		E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC, prob=0.9, threshold=1.0)
 		_, R, t, mask = cv2.recoverPose(E, self.px_cur, self.px_ref, focal=self.focal, pp = self.pp)
-		self.cur_t = self.cur_t + self.cur_R.dot(t) 
+		# absolute_scale = self.getAbsoluteScale(frame_id)
+		self.cur_t = self.cur_t + self.cur_R.dot(t)
 		self.cur_R = R.dot(self.cur_R)
-		# self.cur_t =t
-		# self.cur_R = R
-
+		if(self.px_ref.shape[0] < kMinNumFeature):
+			self.px_cur = self.detector.detect(self.new_frame)
+			self.px_cur = np.array([x.pt for x in self.px_cur], dtype=np.float32)
 		self.px_ref = self.px_cur
-
-	# 类的入口，输入一张图片以及它的序号
+        
 	def update(self, img, frame_id):
 		assert(img.ndim==2 and img.shape[0]==self.cam.height and img.shape[1]==self.cam.width), "Frame: provided image has not the same size as the camera model or image is not grayscale"
-		self.new_frame = self.imgList[frame_id]
+		self.new_frame = img
 		if(self.frame_stage == STAGE_DEFAULT_FRAME):
 			self.processFrame(frame_id)
 		elif(self.frame_stage == STAGE_SECOND_FRAME):
